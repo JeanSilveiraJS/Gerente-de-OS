9441c6b0de6bd7a060cd0c03ffab7304ada60787
JeanSilveiraJS <jeansilveira2500@gmail.com>
2023-09-25 21:39:24 -0300

    commit inicial

---
+++ src/main/java/br/csi/gerentedeos/GerenteDeOsApplication.java
@@ -0,0 +1,13 @@
+package br.csi.gerentedeos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenteDeOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenteDeOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/csi/gerentedeos/GerenteDeOsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.csi.gerentedeos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenteDeOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
