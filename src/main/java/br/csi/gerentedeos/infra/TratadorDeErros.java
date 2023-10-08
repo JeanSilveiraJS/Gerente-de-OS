08894eb1334b392a3c038c14e844b46b2b4c6f0c
JeanSilveiraJS <jeansilveira2500@gmail.com>
2023-10-08 06:27:17 -0300

    edit: tratamento de exceptions

--- src/main/java/br/csi/gerentedeos/infra/TratadorDeErros.java
+++ src/main/java/br/csi/gerentedeos/infra/TratadorDeErros.java
@@ -1,5 +1,7 @@
 package br.csi.gerentedeos.infra;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,19 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class TratadorDeErros {
-    public ResponseEntity tratarErro404(){
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity tratarErro500(Exception e){
-        return ResponseEntity.internalServerError().body(e.getMessage());
-    }
-
-    //Erros de validação
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
         List<FieldError> errors = ex.getFieldErrors();
@@ -38,5 +31,47 @@ public class TratadorDeErros {
         public DadosErroValidacao(FieldError error) {
             this(error.getField(), error.getDefaultMessage());
         }
+    }
+
+    //Erros de validação
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        String details = ex.getCause().getMessage();
+
+        ApiError error = new ApiError(null, "Erro de conscistência de dados");
+
+        //Primary Key
+        if (details.contains("entidade_nome_key")) {
+            error = new ApiError("nome", "Nome já cadastrado");
+        }
+        if (details.contains("entidade_cpf_key")) {
+            error = new ApiError("cpf", "CPF já cadastrado");
+        }
+        if (details.contains("entidade_cnpj_key")) {
+            error = new ApiError("cpf", "CNPJ já cadastrado");
+        }
+        if (details.contains("entidade_email_key")) {
+            error = new ApiError("email", "E-mail já cadastrado");
+        }
+        if (details.contains("entidade_ie_key")) {
+            error = new ApiError("ie", "Inscrição Estadual já cadastrada");
+        }
+
+        //Check
+        if (details.contains("entidade_cpf_cnpj_check")) {
+            error = new ApiError("cpf_cnpj", "Cadastro não deve conter CPF e CNPJ simultaneamente");
+        }
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity tratarErro500(Exception e){
+        return ResponseEntity.internalServerError().body("Erro no Banco de Dados");
     }
 }
